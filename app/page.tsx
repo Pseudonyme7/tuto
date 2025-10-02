dad4f461e04bc968593b3a4ac934ed476de7236b
Julien <aghatorzz@gmail.com>
2025-10-02 14:31:09 +0100

    First component 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+function TableOfContents() {
+  return (
+    <ol>
+      <li>Components: UI Building Blocks</li>
+      <li>Defining a Component</li>
+      <li>Using a Component</li>
+    </ol>
+  );
+}
+
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <article>
+        <h1>My First Component</h1>
+        <TableOfContents />
+      </article>
     </main>
   );
 }
